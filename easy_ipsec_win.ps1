15fc5aef767f6c1dc11bc2c5bd8b9614acf2ae20
daniel.plominski <daniel@plominski.eu>
2015-06-14 10:33:59 +0200

    add powershell support

--- easy_ipsec_win.ps1
+++ easy_ipsec_win.ps1
@@ -66,12 +66,12 @@ Start-Sleep -s 2
 
 
 
-[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing") 
-[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") 
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing1") 
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms1") 
 
-$objForm = New-Object System.Windows.Forms.Form 
+$objForm = New-Object System.Windows.Forms1.Form 
 $objForm.Text = "IPsec PreSharedKey"
-$objForm.Size = New-Object System.Drawing.Size(300,200) 
+$objForm.Size = New-Object System.Drawing1.Size(300,200) 
 $objForm.StartPosition = "CenterScreen"
 
 $objForm.KeyPreview = $True
@@ -80,29 +80,29 @@ $objForm.Add_KeyDown({if ($_.KeyCode -eq "Enter")
 $objForm.Add_KeyDown({if ($_.KeyCode -eq "Escape") 
 {$objForm.Close()}})
 
-$OKButton = New-Object System.Windows.Forms.Button
-$OKButton.Location = New-Object System.Drawing.Size(75,120)
-$OKButton.Size = New-Object System.Drawing.Size(75,23)
+$OKButton = New-Object System.Windows.Forms1.Button
+$OKButton.Location = New-Object System.Drawing1.Size(75,120)
+$OKButton.Size = New-Object System.Drawing1.Size(75,23)
 $OKButton.Text = "OK"
 $OKButton.Add_Click({$x=$objTextBox.Text;$objForm.Close()})
 $objForm.Controls.Add($OKButton)
 
-$CancelButton = New-Object System.Windows.Forms.Button
-$CancelButton.Location = New-Object System.Drawing.Size(150,120)
-$CancelButton.Size = New-Object System.Drawing.Size(75,23)
+$CancelButton = New-Object System.Windows.Forms1.Button
+$CancelButton.Location = New-Object System.Drawing1.Size(150,120)
+$CancelButton.Size = New-Object System.Drawing1.Size(75,23)
 $CancelButton.Text = "Cancel"
 $CancelButton.Add_Click({$objForm.Close()})
 $objForm.Controls.Add($CancelButton)
 
-$objLabel = New-Object System.Windows.Forms.Label
-$objLabel.Location = New-Object System.Drawing.Size(10,20) 
-$objLabel.Size = New-Object System.Drawing.Size(280,20) 
+$objLabel = New-Object System.Windows.Forms1.Label
+$objLabel.Location = New-Object System.Drawing1.Size(10,20) 
+$objLabel.Size = New-Object System.Drawing1.Size(280,20) 
 $objLabel.Text = "Please enter the IPsec PreSharedKey:"
 $objForm.Controls.Add($objLabel) 
 
-$objTextBox = New-Object System.Windows.Forms.TextBox 
-$objTextBox.Location = New-Object System.Drawing.Size(10,60) 
-$objTextBox.Size = New-Object System.Drawing.Size(260,20) 
+$objTextBox = New-Object System.Windows.Forms1.TextBox 
+$objTextBox.Location = New-Object System.Drawing1.Size(10,60) 
+$objTextBox.Size = New-Object System.Drawing1.Size(260,20) 
 $objForm.Controls.Add($objTextBox) 
 
 $objForm.Topmost = $True
